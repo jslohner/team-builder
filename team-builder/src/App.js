f187fc8a5133bfca775fdb30a45273a9d5fea030
Justin Lohner <lohner4@gmail.com>
2020-04-21 13:55:54 -0600

    Created initial list of team members

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -2,10 +2,37 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const initTeamMembers = [
+	{
+		name: 'Justin',
+		age: '19',
+		email: 'lohner4@gmail.com'
+		role: 'Lambda School Student',
+		hobbies: ['golf', 'video games', 'music']
+	},
+	{
+		name: 'Bingus',
+		age: '25',
+		email: 'bingus@bingus.com',
+		role: 'CEO of Starbucks',
+		hobbies: ['golf', 'skiing', 'travelling']
+	},
+	{
+		name: 'Rick',
+		age: 'unknown',
+		email: 'thatsthewaythenewsgoes@plumbus.com',
+		role: 'Smartest man in the universe',
+		hobbies: ['drinking', 'drinking', 'drinking']
+	}
+]
+
 function App() {
+
+	const [teamMembers, setTeamMembers] = useState([]);
+
 	return (
-		<div className="App">
-			<header className="App-header">
+		<div className='App'>
+			<header className='App-header'>
 				<h1>App Header</h1>
 			</header>
 		</div>
@@ -13,3 +40,12 @@ function App() {
 }
 
 export default App;
+
+// const initTeamMembers = [
+// 	{
+// 		name: 'Justin',
+// 		age: '19',
+// 		occupation: 'Lambda School Student',
+// 		hobbies: []
+// 	}
+// ]

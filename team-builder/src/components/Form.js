e9454eaad1571e10deb2a700e69afa2c43eaed61
Justin Lohner <lohner4@gmail.com>
2020-04-21 16:21:16 -0600

    Styled the webpage

--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './ContentStyle.css';
 
 function Form(props) {
 	return (
--- team-builder/src/components/TeamMemberCard.js
+++ team-builder/src/components/TeamMemberCard.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import './ContentStyle.css';
 
 function TeamMemberCard(props) {
 	return (
 		<div className='team-member-card'>
-			<h2 className='member-name'>{props.member.name}</h2>
-			<p className='age'>{props.member.age}</p>
-			<p className='email'>{props.member.email}</p>
-			<p className='role'>{props.member.role}</p>
+			<div className='card-details'>
+				<h2 className='member-name'>Name - {props.member.name}</h2>
+				<p className='age'>Age - {props.member.age}</p>
+				<p className='email'>Email - {props.member.email}</p>
+				<p className='role'>Role - {props.member.role}</p>
+			</div>
 		</div>
 	);
 }

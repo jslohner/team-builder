81cdebc8f42ef1137cd9a489f7a6c638e3619407
Justin Lohner <lohner4@gmail.com>
2020-04-21 15:10:47 -0600

    Added form.js file and created a form for a new team member

--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
-function Form() {
-
+function Form(props) {
+	return (
+		<form className='team-member-form'>
+			<h2>Team Member Form</h2>
+			<label>Name: <input value={props.name} onChange={props.inputChange} name='name' type='text'/></label>
+			<label>Age: <input value={props.age} onChange={props.inputChange} name='age' type='text'/></label>
+			<label>Email: <input value={props.email} onChange={props.inputChange} name='email' type='text'/></label>
+			<label>Role: <input value={props.role} onChange={props.inputChange} name='role' type='text'/></label>
+			<button onClick={props.submit}>Submit</button>
+		</form>
+	);
 }
 
 export default Form;
+
+// hobbies
